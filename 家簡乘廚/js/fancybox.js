28ac23bf1327295d7d695997e2e9d87aa4444226
lincharlie8 <lincharlie1@yahoo.co.jp>
2015-04-14 09:56:55 +0800

    fancybox test

---
+++ 家簡乘廚/js/fancybox.js
@@ -0,0 +1,16 @@
+$(function(){
+
+
+	$(".various").fancybox({
+				maxWidth	: 800,
+				maxHeight	: 700,
+				fitToView	: true,
+				width:'100%',
+				height:'100%',
+				autoSize	: true,
+				closeClick	: false,
+				openEffect	: 'none',
+				closeEffect	: 'none',
+				scrollbar:'auto'
+			});
+});
